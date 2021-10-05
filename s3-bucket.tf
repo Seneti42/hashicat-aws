61b5b87666cb8c4b4b5e364a76345881570ff77a
Dan Ribeiro <dan.r.ribeiro.89@gmail.com>
2021-10-05 11:26:53 +0000

    fixing s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,10 +2,8 @@ module "s3_bucket" {
   source  = "app.terraform.io/Seneti/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "${var.prefix}-s3-bucket"
   acl    = "private"
-
-  bucket_prefix = "${var.prefix}"
 
   versioning = {
     enabled = true
